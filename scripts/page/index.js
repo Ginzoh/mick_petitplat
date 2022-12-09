cbfa8adbf73ad5ee098b726bfc7a0c1502dc3f2a
Mickael <mickael.krauth@epita.fr>
2022-12-09 16:21:23 +0100

    testing

--- scripts/page/index.js
+++ scripts/page/index.js
@@ -52,20 +52,37 @@ function searchPlat(para, tag) {
 }
 
 function trierPlats(plat, para) {
+  let plats = [];
   if (para === "full") {
-    return recipes.filter(function (a) {
-      return (
-        a.description.toUpperCase().search(plat) !== -1 ||
-        a.name.toUpperCase().search(plat) !== -1 ||
-        testIng(a.ingredients, plat)
-      );
-    });
+    console.log("nani");
+    // return recipes.filter(function (a) {
+    //   return (
+    //     a.description.toUpperCase().search(plat) !== -1 ||
+    //     a.name.toUpperCase().search(plat) !== -1 ||
+    //     testIng(a.ingredients, plat)
+    //   );
+    // });
+    for (const el of recipes) {
+      if (
+        el.description.toUpperCase().includes(plat) ||
+        el.name.toUpperCase().includes(plat) ||
+        testIng(el.ingredients, plat)
+      ) {
+        plats.push(el);
+      }
+    }
   }
   if (para === "ing") {
-    return result.filter(function (a) {
-      return testIng(a.ingredients, plat);
-    });
+    // return result.filter(function (a) {
+    //   return testIng(a.ingredients, plat);
+    // });
+    for (const a of result) {
+      if (testIng(a.ingredients, plat)) {
+        plats.push(a);
+      }
+    }
   }
+  return plats;
 }
 function testIng(ingredients, plat) {
   // ingredients.forEach((food) => {
@@ -74,9 +91,15 @@ function testIng(ingredients, plat) {
   //     return true;
   //   }
   // });
-  const found = ingredients.find((ing) =>
-    ing.ingredient.toUpperCase().includes(plat)
-  );
+  let found = [];
+  // = ingredients.find((ing) =>
+  //   ing.ingredient.toUpperCase().includes(plat)
+  // );
+  for (const ing of ingredients) {
+    if (ing.ingredient.toUpperCase().includes(plat)) {
+      found.push(ing);
+    }
+  }
   return found !== undefined;
 }
 
